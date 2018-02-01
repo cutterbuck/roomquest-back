2f75b3e12294ef2537c8ddbd461ecd16ea2553bc
Jake <jakemacnaughton@gmail.com>
2018-02-01 09:52:43 -0500

    added vacant rooms to serializer; built custom backend methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,21 @@ class User < ApplicationRecord
   has_many :user_friends
   has_many :friends, class_name: "User", through: :user_friends
   has_one :vacancy
+
+
+  def associations
+    associations = []
+    self.friends.map do |friend|
+      friend.friends.each do |fof|
+        associations << fof
+      end
+    end
+    associations.delete_if{|el| el.name == self.name}.uniq
+    associations
+  end
+
+  def vacant_rooms
+    rooms = self.associations.map{|ass| ass.vacancy}
+    rooms
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :current_lat, :current_long, :profile_image_url
+  attributes :id, :name, :email, :current_lat, :current_long, :profile_image_url, :vacant_rooms
 
   has_one :vacancy
   has_many :friends, class_name: "User", through: :user_friends
